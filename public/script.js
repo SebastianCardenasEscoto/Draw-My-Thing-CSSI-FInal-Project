0615911c0c8d1ef3892b680fbbc7f7fd09abf10c
Glitch (tangy-crocus-license) <none>
2020-07-28 22:42:29 +0000

    🛥👏 Checkpoint ./public/script.js:4819723/812:18465608/437 ./views/index.html:4819723/60:18465608/62 ./server.js:4819723/192

--- public/script.js
+++ public/script.js
@@ -1,14 +1,13 @@
-/*createCanvas, io.connect, background, ellipse, mouseX, mouseY, line, pmouseX, pmouseY*/
+/*global createCanvas, io.connect, background, ellipse, mouseX, mouseY, line, pmouseX, pmouseY, io, noStroke, keyCode,
+mouseIsPressed, stroke, strokeWeight*/
 
 const SPACEBAR = 32;
-let img  , i;
+let img ;
 let socket;
-let x,y,w,h;
+let guessForm = document.getElementById("guessForm");
 
 // importimg image
-function preload() {
-  img = loadImage('https://cdn.glitch.com/788491d6-3d68-4b92-871c-448f512a6761%2Fheart.png?v=1595961536085');
-}
+
 
 function setup(){
   
@@ -18,41 +17,23 @@ function setup(){
   socket = io.connect();
   background(51);
   noStroke();
-  wx= 30 ;        
-  wy = 40;
-  ww = 120;
-  wh = 110;
- // create a tex box  
-textSize(15);
- // text color
-fill("white");
-// the actual text
-text('Waiting for players...', 10, 30);
-  
-// where the image is going to be placed on the screen
-  img.loadPixels();
-  i  = img.get(img.width / 2, img.height / 2);
+ 
+
 }
 
 function draw()  {
-// The created rectangle properties
-  fill(13, 115, 105);
-  noStroke();
-  rect(wx,wy,ww,wh,20);
-  
-  // The Imported image background
-  background(i);
-  image(img, 45, 60, 40, 40);
-  
-  
-  
-  
-  
-  // Background for the web page.
+
   if(mouseIsPressed){
     stroke('white');
-    strokeWeight(20);
+    strokeWeight(10);
     line(mouseX,mouseY,pmouseX,pmouseY);
+    
+    let mousePosition = {
+      x: mouseX,
+      y: mouseY
+    };
+    
+    socket.emit("mouse", mousePosition);
   }
   
 }
@@ -73,3 +54,13 @@ class PaintBrush{
     this.strokeWidth;
   }
 }
+
+guessForm.addEventListener("submit", (e)=>{
+  e.preventDefault();
+  let msg = e.target.elements.msg.value;
+  
+  socket.emit('guess', msg);
+  
+  e.target.elements.msg.value = '';
+  e.target.elements.msg.focus();
+});
--- server.js
+++ server.js
@@ -28,9 +28,13 @@ var io = socket(listener);
 
 // runs when new user connects
 io.sockets.on('connection', (socket) =>{
-  console.log('new connection: ' + socket.id);
   users.push(socket.id);
-  console.log(users);
+  
+  socket.on("guess", msg => console.log(msg));
+  
+  socket.on("mousePosition", ()=>{
+    
+  });
 });
 
 // runs when user disconnects

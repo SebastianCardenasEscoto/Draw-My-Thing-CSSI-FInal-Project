e05047b42e1dea6a210b3628ae73d4f551ca447c
Glitch (tangy-crocus-license) <none>
2020-07-29 04:37:16 +0000

    💃🙆 Checkpoint ./public/script.js:18465608/3664

--- public/script.js
+++ public/script.js
@@ -7,12 +7,16 @@ let colorPicker;
 let socket;
 let guessForm = document.getElementById("guessForm");
 let drawingForm = document.getElementById("sketch-div");
-var timeleft = 60;
-
+var timeleft = 90;
 var startTime = 0;
 var currentTime = 0;
-
+var min = floor(s / 60);
+var sec = s % 60;
 var ding;
+var params = getURLParams();
+var min = params.minute;
+var timer = select('#timer');
+var interval = setInterval(timeIt, 1000);
 
 // importimg image
 
@@ -36,6 +40,12 @@ function setup(){
 }
 
 function draw()  {
+
+  
+  
+  
+  
+  
   // This fucntion allows the background to change to whatever color is selected.\
   if(backgroundColor != colorPicker.color()) backgroundColor = colorPicker.color();
   drawingForm.style.backgroundColor = backgroundColor;
@@ -91,41 +101,3 @@ function drawLine(x,y,px,py){
     line(x,y,px,py);
 }
 
-
-function convertSeconds(s) {
-  var min = floor(s / 60);
-  var sec = s % 60;
-  return nf(min, 2) + ':' + nf(sec, 2);
-}
-
-
-function preload() {
-  ding = loadSound("ding.mp3");
-}
-
-  startTime = millis();
-
-
-  var params = getURLParams();
-  console.log(params);
-  if (params.minute) {
-    var min = params.minute;
-    timeleft = min * 60;
-  }
-
-  var timer = select('#timer');
-  timer.html(convertSeconds(timeleft - currentTime));
-
-  var interval = setInterval(timeIt, 1000);
-
-  function timeIt() {
-    currentTime = floor((millis() - startTime) / 1000);
-    timer.html(convertSeconds(timeleft - currentTime));
-    if (currentTime == timeleft) {
-      ding.play();
-      clearInterval(interval);
-      //counter = 0;
-    }
-
-
-}
